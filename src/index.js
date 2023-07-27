1a0401f6d1c6d8b4861d805f76c2a22f1b76e697
Shabbar <shabbar3@hotmail.com>
2023-07-27 15:05:16 -0400

    add new fags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log ("hello")
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/api', (req, res) => {
+  res.send('hello')
+})
+app.get('/', (req, res) => { res.send('') })
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
