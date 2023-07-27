de1e8ace764dc4d6d8c4cba0a1deb78521012da1
Shabbar <shabbar3@hotmail.com>
2023-07-27 15:21:01 -0400

    time

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ const app = express()
 const port = 80
 
 app.get('/api', (req, res) => {
-  res.send('hello')
+    res.send('dickballs dynamic')
 })
-app.get('/', (req, res) => { res.send('') })
+app.get('/api/time', (req, res) => {
+    res.send(new Date().getTime())
+})
 
+// azure health check
+app.get('/', (req, res) => { res.send('') })
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
